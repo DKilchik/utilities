ce62692e7bb58f9b43c515e1ba1484339d20b4a1
Dmitry Kilchik <dmitry.kylchik@instatsport.email>
2022-03-09 19:19:11 +0200

    implicitly wait

--- webdriver/web/webdriver_extension.py
+++ webdriver/web/webdriver_extension.py
@@ -2,9 +2,10 @@
 
 class WebdriverExtension:
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self, driver, implicitly_wait=5):
+        self._driver = driver
         self.url = None
+        self._driver.implicitly_wait(implicitly_wait)
 
     def open(self):
-        self.driver.get(self.url)
+        self._driver.get(self.url)
